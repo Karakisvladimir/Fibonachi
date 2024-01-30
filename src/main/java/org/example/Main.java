be4bb9d4c5694196cf62d3faf5910662403b0d23
Vladimir <karakisvladimir@gmail.com>
2024-01-30 21:48:16 +0200

    mistake

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,11 +3,10 @@ package org.example;
 import fibonachi.DinamicProgramuvannia;
 import fibonachi.Iteraziia;
 import fibonachi.Recursia;
-import java.util.*;
+
 import java.util.Scanner;
 
-// Press ⇧ twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
